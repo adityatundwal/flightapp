625fd8aef81a13966ff6f8c39031eabc3e102ac4
Aditya Tundwal <aditya_tundwal_@outlook.com>
2023-10-16 00:11:38 +0530

    dd

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,18 @@ void main() {
 
 class flightapp extends StatelessWidget {
   MaterialColor mycolor = MaterialColor(
-    0xff00406c,
+    0xffEE82EE,
     <int, Color>{
-      50: Color(0xff00406c),
-      100: Color(0xff00406c),
-      200: Color(0xff00406c),
-      300: Color(0xff00406c),
-      400: Color(0xff00406c),
-      500: Color(0xff00406c),
-      600: Color(0xff00406c),
-      700: Color(0xff00406c),
-      800: Color(0xff00406c),
-      900: Color(0xff00406c),
+      50: Color(0xffEE82EE),
+      100: Color(0xffEE82EE),
+      200: Color(0xffEE82EE),
+      300: Color(0xffEE82EE),
+      400: Color(0xffEE82EE),
+      500: Color(0xffEE82EE),
+      600: Color(0xffEE82EE),
+      700: Color(0xffEE82EE),
+      800: Color(0xffEE82EE),
+      900: Color(0xffEE82EE),
     },
   );
   flightapp({super.key});
